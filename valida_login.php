10faece7fa6c18ac8d9585303897a8d09dc33935
emanoelanrs <emanoelaneris@gmail.com>
2023-07-10 19:12:23 -0300

    primeiro commit

---
+++ valida_login.php
@@ -0,0 +1,18 @@
+<?php
+/*
+print_r($_GET); //GET expoe o paramentro de um formulário na url
+
+echo '<br/>';
+
+echo $_GET['email'];
+echo $_GET ['senha'];
+*/
+
+   print_r ($_POST); // é um array, pode ser acessado seus indices
+   echo '<br/>'; 
+
+   echo $_POST['email']; //pagina de envio de dados
+   echo $_POST['senha'];
+
+
+?>
